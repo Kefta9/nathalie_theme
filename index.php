c652c0d29206602640e4e8862a9acdb1e8237744
Kevin <Vincent.kevin@outlook.com>
2025-08-18 18:40:02 +0200

    Sauvegarde

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ get_header();
 	<section class="home-content">
 		<h1>Bienvenue sur le site de Nathalie Mota</h1>
 		<p>Photographe freelance - Portfolio, réservation et achat de photos</p>
-		<!-- Ajoute ici ton contenu principal -->
 	</section>
 </main>
 
