424ee5dffa9b600fef34d098759c44e229669025
Kevin <Vincent.kevin@outlook.com>
2025-09-11 18:02:48 +0200

    Ajout de la hero section sur la page d'accueil

--- index.php
+++ index.php
@@ -3,9 +3,22 @@ get_header();
 ?>
 
 <main id="main" class="site-main" role="main">
-	<section class="home-content">
-		<h1>Bienvenue sur le site de Nathalie Mota</h1>
-		<p>Photographe freelance - Portfolio, réservation et achat de photos</p>
+	<section class="home-hero">
+		<?php
+		$random_photos = get_photos_by_category([], 1, [], true);
+		if (!empty($random_photos)) :
+			$post = $random_photos[0];
+			setup_postdata($post);
+			$bg_url = get_the_post_thumbnail_url($post->ID, 'full');
+		?>
+			<div class="home-hero__bg">
+				<img src="<?php echo esc_url($bg_url); ?>" alt="Image de fond aléatoire" />
+			</div>
+		<?php wp_reset_postdata(); endif; ?>
+
+		<div class="home-hero__content">
+			<h1 class="hero__title">Photographe Event</h1>
+		</div>
 	</section>
 </main>
 
