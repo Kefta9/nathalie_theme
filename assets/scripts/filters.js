080e7c9a5bef5c72184a766b460cfba94c0d489a
Kevin <Vincent.kevin@outlook.com>
2025-09-18 15:30:35 +0200

    Correctif CSS + Ajout condition filters.js

--- assets/scripts/filters.js
+++ assets/scripts/filters.js
@@ -1,15 +1,17 @@
 let currentPage = 1;
 const perPage = 8;
 
-function loadPhotos(reset = false) { // reset = true remplace le contenu, false ajoute à la fin
+function loadPhotos(reset = false) {
+  // reset = true remplace le contenu, false ajoute à la fin
   const categorie = document.querySelector("input[name='categorie']").value; // valeur renvoyé par dataset.value du dropdown.js
   const format = document.querySelector("input[name='format']").value;
   const order = document.querySelector("input[name='order']").value;
 
-  fetch(nathalie_ajax.ajax_url, { // défini dans wp_localize_script
+  fetch(nathalie_ajax.ajax_url, {
+    // défini dans wp_localize_script
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams({ 
+    body: new URLSearchParams({
       action: "load_more_photos", // action à exécuter
       nonce: nathalie_ajax.nonce,
       page: currentPage,
@@ -21,8 +23,9 @@ function loadPhotos(reset = false) { // reset = true remplace le contenu, false 
     .then((res) => res.json())
     .then((data) => {
       const grid = document.querySelector(".other-photos__grid");
-      if (data.success) { 
-        if (reset) { // Si changement de filtre, on remplace le contenu
+      if (data.success) {
+        if (reset) {
+          // Si changement de filtre, on remplace le contenu
           grid.innerHTML = data.data;
         } else {
           grid.insertAdjacentHTML("beforeend", data.data);
@@ -34,10 +37,13 @@ function loadPhotos(reset = false) { // reset = true remplace le contenu, false 
 }
 
 // Bouton charger plus
-document.getElementById("load-more").addEventListener("click", () => {
-  currentPage++;
-  loadPhotos();
-});
+const loadMoreBtn = document.getElementById("load-more");
+if (loadMoreBtn) {
+  loadMoreBtn.addEventListener("click", () => {
+    currentPage++;
+    loadPhotos();
+  });
+}
 
 // Changement de filtres
 document
@@ -48,4 +54,4 @@ document
       loadPhotos(true); // recharge avec reset car changement de filtre
       document.getElementById("load-more").style.display = "block";
     });
-});
+  });
